f832469bf33c406dad0eb9a5910b89e5cd312bd8
zlatakjshkarova <147971125+zlatakjshkarova@users.noreply.github.com>
2024-11-08 20:40:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+for (let r = 1; r < 10; r++) {
+    console.log(r)
+}
+
+let num = 1;
+while (num < 10) {
+    console.log(num)
+} num++
+
+
+
+for (let w = 0; w < 20; r++) {
+    if (i % 2 == 0) continue;
+    console.log(w)
+}
+
+for (let d = 0; d < 30; w++) {
+    let pass = promot('ваше имя');
+    if (pass == "qjs") {
+        alert("ваш год рождения");
+
+
+    } else {
+        alert("сколько вам лет")
+    }
+
+}
+
+for (let h = 0; h < 2000; h++) {
+    if (h % 5 == 0) continue;
+    console.log(h)
+}
+
+for (let k = 0; h < 5000; k++) {
+    if (k % 10 == 0) continue;
+    console.log(k)
+}
+for (let n = 0; n < 10000; n++) {
+    if (n % 5 == 0) continue;
+    console.log(n)
+}
